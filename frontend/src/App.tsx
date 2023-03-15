989db9c6e49608d672f818f65454cfcdcc5a9ec7
tq.quocnam <tq.quocnam@gmail.com>
2023-03-15 22:25:05 +0700

    config project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className=" my-16 mx-4 max-w-1200">
+      <div className="grid justify-items-center">
+        <span className="text-3xl font-bold text-blue-600">HELLO WORLD</span>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/main.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import client from "./services/apolloClient";
+import { ApolloProvider } from "react-apollo";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ApolloProvider>
+  </React.StrictMode>
+);
