ad0dfc8361c0e6953b1f1a7a129da91893e81efd
tq.quocnam <tq.quocnam@gmail.com>
2023-04-10 21:08:20 +0700

    fix error get query missing on strapi

--- strapi/config/plugins.js
+++ strapi/config/plugins.js
@@ -14,5 +14,18 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  graphql: {
+    enabled: true,
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: true,
+      defaultLimit: 10,
+      maxLimit: 20,
+      apolloServer: {
+        tracing: true,
+      },
+    },
+  },
   // ...
 });
